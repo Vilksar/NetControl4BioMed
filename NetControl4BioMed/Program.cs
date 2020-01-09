b07e3f51ab94ee3c29777873ad67b63fd23ac4f1
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2020-01-09 11:01:55 +0200

    Updated the main classes.

--- NetControl4BioMed/Program.cs
+++ NetControl4BioMed/Program.cs
@@ -9,18 +9,38 @@ using Microsoft.Extensions.Logging;
 
 namespace NetControl4BioMed
 {
+    /// <summary>
+    /// Represents the main class of the application.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Initializes the application.
+        /// </summary>
+        /// <param name="args">Represents the arguments of the application.</param>
         public static void Main(string[] args)
         {
+            // Create a web host builder, build the host and run it.
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        /// <summary>
+        /// Creates a web host builder with the given arguments.
+        /// </summary>
+        /// <param name="args">Represents the arguments for the web host builder.</param>
+        /// <returns>Returns a new web host as defined in the "Startup" class.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // Return a default web host with the given arguments and parameters.
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.SetMinimumLevel(LogLevel.Warning);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
--- NetControl4BioMed/Startup.cs
+++ NetControl4BioMed/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,42 +12,78 @@ using Microsoft.Extensions.Hosting;
 
 namespace NetControl4BioMed
 {
+    /// <summary>
+    /// Represents the actions to perform by the application at startup.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Gets the configuration options for the application.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the application startup.
+        /// </summary>
+        /// <param name="configuration">Represents the application configuration options.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Configures the services at the application startup.
+        /// </summary>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </remarks>
+        /// <param name="services">Represents the service collection to be configured.</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure the cookie options.
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // Determine whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            // Add Razor pages.
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configures the application options at startup.
+        /// </summary>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </remarks>
+        /// <param name="app">Represents the application builder.</param>
+        /// <param name="env">Represents the hosting environment of the application.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Check the environment in which it is running.
             if (env.IsDevelopment())
             {
+                // Display more details about the errors.
                 app.UseDeveloperExceptionPage();
             }
             else
             {
+                // Redirect to a generic "Error" page.
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // Use re-execution for the HTTP error status codes, to the same "Error" page.
+                app.UseStatusCodePagesWithReExecute("/Error", "?errorCode={0}");
+                // The default HSTS value is 30 days. This may change for production scenarios, as in https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            // Parameters for the application.
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseCookiePolicy();
             app.UseRouting();
-
+            // Use authentication.
             app.UseAuthorization();
-
+            // Use Razor pages.
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
