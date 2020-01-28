3ec68b9ef95911e23f88abf4906679a43ef082c0
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2020-01-28 11:18:59 +0200

    Updated the main shared files.

--- NetControl4BioMed/wwwroot/js/site.js
+++ NetControl4BioMed/wwwroot/js/site.js
@@ -3,5 +3,15 @@
 
 // Write your Javascript code.
 $(window).on('load', () => {
-    
+
+    // Check if there is a cookie notification alert on the page.
+    if ($('.cookie-consent-alert').length !== 0) {
+        // Get the cookie acceptance button.
+        const button = $('.cookie-consent-alert').first().find('.cookie-consent-alert-dismiss');
+        // Add a listener for clicking the button.
+        $(button).on('click', () => {
+            document.cookie = button.data('cookie-consent-string');
+        });
+    }
+
 });
