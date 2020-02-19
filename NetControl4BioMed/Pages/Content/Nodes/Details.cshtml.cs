eee992d165ead4d853fc2920472193f9651ea417
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2020-02-19 16:03:01 +0200

    Added the content nodes pages.

---
+++ NetControl4BioMed/Pages/Content/Nodes/Details.cshtml.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NetControl4BioMed.Data;
+using NetControl4BioMed.Data.Models;
+
+namespace NetControl4BioMed.Pages.Content.Nodes
+{
+    [Authorize]
+    public class DetailsModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(UserManager<User> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public ViewModel View { get; set; }
+
+        public class ViewModel
+        {
+            public Node Node { get; set; }
+
+            public IEnumerable<DatabaseNodeFieldNode> DatabaseNodeFieldNodes { get; set; }
+
+            public IEnumerable<Edge> Edges { get; set; }
+
+            public IEnumerable<NodeCollection> NodeCollections { get; set; }
+        }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            // Get the current user.
+            var user = await _userManager.GetUserAsync(User);
+            // Check if the user does not exist.
+            if (user == null)
+            {
+                // Display a message.
+                TempData["StatusMessage"] = "Error: An error occured while trying to load the user data. If you are already logged in, please log out and try again.";
+                // Redirect to the home page.
+                return RedirectToPage("/Index");
+            }
+            // Check if there isn't any ID provided.
+            if (string.IsNullOrEmpty(id))
+            {
+                // Display a message.
+                TempData["StatusMessage"] = "Error: No ID has been provided.";
+                // Redirect to the index page.
+                return RedirectToPage("/Content/Nodes/Index");
+            }
+            // Get the item with the provided ID.
+            var item = _context.Nodes
+                .Where(item => !item.DatabaseNodes.Any(item1 => item1.Database.DatabaseType.Name == "Generic"))
+                .Where(item => item.Id == id)
+                .Include(item => item.DatabaseNodeFieldNodes)
+                    .ThenInclude(item => item.DatabaseNodeField)
+                        .ThenInclude(item => item.Database)
+                .Include(item => item.EdgeNodes)
+                    .ThenInclude(item => item.Edge)
+                        .ThenInclude(item => item.DatabaseEdges)
+                            .ThenInclude(item => item.Database)
+                                .ThenInclude(item => item.DatabaseUsers)
+                .Include(item => item.NodeCollectionNodes)
+                    .ThenInclude(item => item.NodeCollection)
+                .FirstOrDefault();
+            // Check if there was no item found.
+            if (item == null)
+            {
+                // Display a message.
+                TempData["StatusMessage"] = "Error: No item has been found with the provided ID.";
+                // Redirect to the index page.
+                return RedirectToPage("/Content/Nodes/Index");
+            }
+            // Define the view.
+            View = new ViewModel
+            {
+                Node = item,
+                DatabaseNodeFieldNodes = item.DatabaseNodeFieldNodes,
+                Edges = item.EdgeNodes
+                    .Select(item => item.Edge)
+                    .Distinct()
+                    .Where(item => !item.DatabaseEdges.Any(item1 => item1.Database.DatabaseType.Name == "Generic"))
+                    .Where(item => item.DatabaseEdges.Any(item1 => item1.Database.IsPublic || item1.Database.DatabaseUsers.Any(item2 => item2.User == user))),
+                NodeCollections = item.NodeCollectionNodes
+                    .Select(item => item.NodeCollection)
+                    .Distinct()
+            };
+            // Return the page.
+            return Page();
+        }
+    }
+}
