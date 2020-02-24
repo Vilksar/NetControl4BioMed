c178a5fa9a311309d9c0f49a62a64c15e8c94b24
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2020-02-24 11:26:20 +0200

    Updated the file and folder structure for the content pages.

---
+++ NetControl4BioMed/Pages/Content/Data/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using NetControl4BioMed.Data.Models;
+
+namespace NetControl4BioMed.Pages.Content.Data
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            // Redirect to the index page.
+            return RedirectToPage("/Content/Index");
+        }
+    }
+}
