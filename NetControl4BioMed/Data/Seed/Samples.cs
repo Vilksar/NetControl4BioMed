e752399a8b00bf6c120dda47f0cc400b7934d873
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2020-11-30 12:45:35 +0200

    Fixed small inconsistencies.

--- NetControl4BioMed/Data/Seed/Samples.cs
+++ NetControl4BioMed/Data/Seed/Samples.cs
@@ -32,7 +32,7 @@ namespace NetControl4BioMed.Data.Seed
                 Name = "Tutorial generic analysis source nodes",
                 Description = "These are the source nodes used by the generic analysis created in the tutorial. They represent a set of randomly chosen 7 nodes from the list of nodes in the tutorial generic network.",
                 Type = Enumerations.SampleType.SourceNodes,
-                Data = "node4\nnode8\nnode9"
+                Data = "node4\nnode8\nnode9\n"
             },
             new Sample
             {
@@ -48,9 +48,9 @@ namespace NetControl4BioMed.Data.Seed
                 Id = "24f8e55a-2b26-45b6-8ec8-49a8c3a9cbc1",
                 DateTimeCreated = DateTime.UtcNow,
                 Name = "Tutorial PPI network seed nodes",
-                Description = "These are the seed nodes used by the PPI network created in the tutorial. They represent a set of randomly selected protein identifiers. They represent a set of randomly chosen 20 nodes from the list of nodes in the tutorial PPI network.",
+                Description = "These are the seed nodes used by the PPI network created in the tutorial. They represent a set of randomly chosen 10 protein identifiers.",
                 Type = Enumerations.SampleType.SeedNodes,
-                Data = "EGFR\nYES1\nMAPK3\nACE2\nSOD1\nPPARG\nPTPN1\nSLC2A1\nFLT1\nERBB2"
+                Data = "EGFR\nYES1\nMAPK3\nACE2\nSOD1\nPPARG\nPTPN1\nSLC2A1\nFLT1\nERBB2\n"
             },
             new Sample
             {
