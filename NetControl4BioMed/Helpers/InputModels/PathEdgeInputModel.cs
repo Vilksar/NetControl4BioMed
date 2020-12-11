f4b24517a8d57dc6a3de1a54923fd4dce0708f28
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2020-12-11 13:48:10 +0200

    Added the new data input models.

---
+++ NetControl4BioMed/Helpers/InputModels/PathEdgeInputModel.cs
@@ -0,0 +1,23 @@
+﻿namespace NetControl4BioMed.Helpers.InputModels
+{
+    /// <summary>
+    /// Represents the input model for a path edge.
+    /// </summary>
+    public class PathEdgeInputModel
+    {
+        /// <summary>
+        /// Represents the network of the path edge.
+        /// </summary>
+        public PathInputModel Path { get; set; }
+
+        /// <summary>
+        /// Represents the edge of the path edge.
+        /// </summary>
+        public EdgeInputModel Edge { get; set; }
+
+        /// <summary>
+        /// Gets or sets the index of the path edge.
+        /// </summary>
+        public int Index { get; set; }
+    }
+}
---
+++ NetControl4BioMed/Helpers/InputModels/PathInputModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace NetControl4BioMed.Helpers.InputModels
+{
+    /// <summary>
+    /// Represents the input model for a path.
+    /// </summary>
+    public class PathInputModel
+    {
+        /// <summary>
+        /// Represents the ID of the path.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Represents the description of the edge.
+        /// </summary>
+        public ControlPathInputModel ControlPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the nodes which appear in the path.
+        /// </summary>
+        public IEnumerable<PathNodeInputModel> PathNodes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the edges which appear in the path.
+        /// </summary>
+        public IEnumerable<PathEdgeInputModel> PathEdges { get; set; }
+    }
+}
