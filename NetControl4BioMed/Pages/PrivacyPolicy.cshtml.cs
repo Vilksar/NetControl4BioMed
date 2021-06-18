f7763cc3594bf6e50c5afc9b3cd533d4b1daa78c
Victor Popescu <19798302+vilksar@users.noreply.github.com>
2021-06-18 10:47:58 +0300

    Added the shared and home pages.

---
+++ NetControl4BioMed/Pages/PrivacyPolicy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace NetControl4BioMed.Pages
+{
+    [AllowAnonymous]
+    public class PrivacyPolicyModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            // Return the page.
+            return Page();
+        }
+    }
+}
--- NetControl4BioMed/wwwroot/js/site.js
+++ NetControl4BioMed/wwwroot/js/site.js
@@ -4,6 +4,33 @@
 // Wait for the window to load.
 $(window).on('load', () => {
 
+    // Check if there is a cookie notification alert on the page.
+    if ($('.cookie-consent-alert').length !== 0) {
+        // Get the cookie acceptance button.
+        const button = $('.cookie-consent-alert').first().find('.cookie-consent-alert-dismiss');
+        // Add a listener for clicking the button.
+        $(button).on('click', () => {
+            document.cookie = button.data('cookie-consent-string');
+        });
+    }
 
+    // Check if there is a UTC date on the page.
+    if ($('.utc-date').length !== 0) {
+        // Define a function to update an element containing a UTC date.
+        const updateDate = (element) => {
+            // Get the date in UTC format.
+            const date = new Date($(element).data('date'));
+            // Update the UTC date to the local date.
+            $(element).find('.utc-date-date').attr('title', date.toLocaleDateString());
+            $(element).find('.utc-date-date').text(date.toLocaleDateString());
+            $(element).find('.utc-date-time').attr('title', date.toLocaleTimeString());
+            $(element).find('.utc-date-time').text(date.toLocaleTimeString());
+        };
+        // Execute the function on page load.
+        (() => {
+            // Update all elements containing UTC dates.
+            $('.utc-date').each((index, element) => updateDate(element));
+        })();
+    }
 
 });
