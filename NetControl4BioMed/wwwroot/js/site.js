ac2f6888431ead7c753c5527192021c4dc90cb3a
Victor Popescu <19798302+vilksar@users.noreply.github.com>
2021-06-21 12:18:24 +0300

    Fixed small bugs and inconsistencies.

--- NetControl4BioMed/wwwroot/js/site.js
+++ NetControl4BioMed/wwwroot/js/site.js
@@ -33,4 +33,100 @@ $(window).on('load', () => {
         })();
     }
 
+    // Check if there is a datatable on the page.
+    if ($('.table-datatable').length !== 0) {
+        // Go over each datatable.
+        $('.table-datatable').each((index, element) => {
+            // Format the table as datatable.
+            const table = $(element).DataTable({
+                'autoWidth': false
+            });
+            // Get the index of the index column.
+            const columnIndex = table.column('index:name').index();
+            // Check if there is any index column.
+            if (typeof (columnIndex) !== 'undefined') {
+                // Add a listener for ordering or searching in the table.
+                table.on('order.dt search.dt', () => {
+                    // Update the corresponding column on searching or ordering.
+                    table.column(columnIndex, { search: 'applied', order: 'applied' }).nodes().each((cell, index) => {
+                        $(cell).find('span').html(index + 1);
+                    });
+                }).draw();
+            }
+        });
+    }
+
+    // Check if there is an item group on the page.
+    if ($('.item-group').length !== 0) {
+        // Define a function which gets all of the selected items and creates a JSON string array with their IDs.
+        const updateSelectedItems = (groupElement) => {
+            // Get all of the list group items.
+            const items = $(groupElement).find('.item-group-item');
+            // Go over all of the checked elements and get the corresponding list group items.
+            const selectedItems = $(groupElement).find('.item-group-item-checkbox:checked').closest('.item-group-item');
+            // Go over all of the unchecked elements and get the corresponding list group items.
+            const unselectedItems = $(groupElement).find('.item-group-item-checkbox:not(:checked)').closest('.item-group-item');
+            // Go over all of the selected items and check all of the checkboxes.
+            $(selectedItems).find('input[type="checkbox"]:not(:checked)').prop('checked', true);
+            // Go over all of the unselected items and uncheck all of the checkboxes.
+            $(unselectedItems).find('input[type="checkbox"]:checked').prop('checked', false);
+            // Go over all of the selected items and mark them as active.
+            $(selectedItems).addClass('table-active');
+            // Go over all of the unselected items and mark them as not active.
+            $(unselectedItems).removeClass('table-active');
+            // Check how many elements are selected.
+            if (selectedItems.length === 0) {
+                // Disable the group buttons.
+                $('.item-group-button').prop('disabled', true);
+                // Unmark the checkbox as indeterminate.
+                $(groupElement).find('.item-group-select').prop('indeterminate', false);
+                // Uncheck the checkbox.
+                $(groupElement).find('.item-group-select').prop('checked', false);
+            } else {
+                // Enable the group buttons.
+                $('.item-group-button').prop('disabled', false);
+                // Check if not all elements are selected.
+                if (selectedItems.length !== items.length) {
+                    // Mark the checkbox as indeterminate.
+                    $(groupElement).find('.item-group-select').prop('indeterminate', true);
+                } else {
+                    // Unmark the checkbox as indeterminate.
+                    $(groupElement).find('.item-group-select').prop('indeterminate', false);
+                    // Check the checkbox.
+                    $(groupElement).find('.item-group-select').prop('checked', true);
+                }
+            }
+        };
+        // Add a listener for when a checkbox gets checked or unchecked.
+        $('.item-group').on('change', '.item-group-item-checkbox', (event) => {
+            // Get the current list group.
+            const groupElement = $(event.target).closest('.item-group');
+            // Update the selected items.
+            updateSelectedItems(groupElement);
+        });
+        // Add a listener for the select checkbox.
+        $('.item-group-select').on('change', (event) => {
+            // Get the current list group.
+            const groupElement = $(event.target).closest('.item-group');
+            // Check if the checkbox is currently checked.
+            if ($(event.target).prop('checked')) {
+                // Check all of the checkboxes on the page.
+                $(groupElement).find('.item-group-item-checkbox').prop('checked', true);
+            } else {
+                // Uncheck all of the checkboxes on the page.
+                $(groupElement).find('.item-group-item-checkbox').prop('checked', false);
+            }
+            // Update the selected items.
+            updateSelectedItems(groupElement);
+        });
+        // On page load, parse the input and check the group items.
+        (() => {
+            // Go over all of the groups.
+            $('.item-group').each((index, element) => {
+                // Update the selected items.
+                updateSelectedItems(element);
+            });
+        })();
+    }
+
 });
